1b6e667a907116e00a84e9064ec1d2bd57279a85
olive <olivehasan99@gmail.com>
2026-03-28 18:04:51 +0100

    added documentation

--- obj/Debug/net8.0/allergens_extractor.AssemblyInfo.cs
+++ obj/Debug/net8.0/allergens_extractor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("allergens_extractor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e372d36ea6807228fea16679f417d70718634136")]
 [assembly: System.Reflection.AssemblyProductAttribute("allergens_extractor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("allergens_extractor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
